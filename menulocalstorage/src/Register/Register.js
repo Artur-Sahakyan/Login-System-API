50070c6edabb0fdbed137ff0d410a37122960799
Artur Sahakyan <artursahakyan2019@mail.ru>
2022-07-16 01:27:09 +0400

    Commited new changes from local

--- menulocalstorage/src/Register/Register.js
+++ menulocalstorage/src/Register/Register.js
@@ -1,26 +1,46 @@
 import "./register.css"
+import {useState} from "react"
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+// let func = function (){
+
+//     let [path, setPath] = useState("/");
+//     const username = document.getElementById("username").value;
+
+//     if(username === "aaa"){
+//         setPath("singIn")
+//     } 
+// }
+
+
 function Register (){
+
+    // let [path, setPath] = useState("/");
+    // const username = document.getElementById("username").value;
+
+    // alert(username)
+    // alert(path)
     return (
         <div className="form"  >
             <h1>Registration</h1>
             <form>
-            <div className="input-container" >
-                <label> Username </label>
-                <input type="text" name="uname" id="username" /> 
+            <label> Username </label>
 
+            <div className="input-container" >
+                <input type="text" name="uname" id="username" /> 
             </div>
-            <div className="input-container">
-                <label>Password </label>
-                <input type="password" name="pass" id="password" />
-                
+
+            <label>Password </label>
+            <div className="input-container"> 
+                <input type="password" name="pass" id="password" /> 
             </div>
+
             <div className="button-container">
-                
-                <Link to={"singIn"} >
-                    <input type="submit" />
-                </Link>
+
+            <Link className="submit" to="/" >
+               <input type="submit" />
+            </Link>
+            
             </div>
             </form>
         </div>
--- menulocalstorage/src/SingIn/SingIn.js
+++ menulocalstorage/src/SingIn/SingIn.js
@@ -6,15 +6,15 @@ function SingIn (){
         <div className="form"  >
             <h1>Sing In</h1>
             <form>
-            <div className="input-container" >
-                <label> Username </label>
-                <input type="text" name="uname" id="username" /> 
+            <label> Username </label>
 
+            <div className="input-container" >
+                <input type="text" name="uname" id="username" /> 
             </div>
+            
+            <label>Password </label>
             <div className="input-container">
-                <label>Password </label>
                 <input type="password" name="pass" id="password" />
-                
             </div>
             <div className="button-container">
                 
