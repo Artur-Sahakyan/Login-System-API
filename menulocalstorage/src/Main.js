f5b0576c1ea018050234eba3f15f3c244dcbe66d
Narek <narek.petrosyan.js@gmail.com>
2022-07-15 12:10:40 -0700

    fix login sistem

--- menulocalstorage/src/Main.js
+++ menulocalstorage/src/Main.js
@@ -1,23 +1,23 @@
 import './Main.css';
-import GetWeather from './GetWeather/GetWeather';
-import Register from "./Register/Register"
-import SingIn from "./SingIn/SingIn"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GetWeather from './components/GetWeather/GetWeather';
+import Register from "./components/Register/Register"
+import SingIn from "./components/SingIn/SingIn"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as React from "react";
 
+const A = () => <div>Apapapapa</div>
 
 function Main() {
+
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Routes>
-
           <Route path="/" element={<Register />} />
           <Route path="singIn" element={<SingIn />} />
           <Route path="/singIn/getweather" element={<GetWeather />} />
-          
         </Routes>
-      </Router>
+      </BrowserRouter>
 
     </div>
   );
